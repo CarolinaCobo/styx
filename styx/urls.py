0fc156cdc6e70874c7c244a8d8b555816105fc12
Carolina Cobo <ccobo.dev@gmail.com>
2021-09-08 07:33:11 +0100

    feat: add products views and templates

--- styx/urls.py
+++ styx/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 
 
 from django.contrib import admin
-
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
