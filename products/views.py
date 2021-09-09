10b2c22ef2d3c4f44e08cc2f89acbb94fa5c9810
Carolina Cobo <ccobo.dev@gmail.com>
2021-09-09 09:18:21 +0100

    feat: aJS sorting functionality and button to go to top of the page

--- products/views.py
+++ products/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
+
+# Create your views here.
 
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    print(request, "[][][][][")
+
     products = Product.objects.all()
     query = None
     categories = None
@@ -20,7 +24,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -28,7 +33,6 @@ def all_products(request):
             products = products.order_by(sortkey)
 
         if 'category' in request.GET:
-
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
