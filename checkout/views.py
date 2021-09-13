c63ad5098aa844a4247adcb6ba678df1a4ddbf5f
Carolina Cobo <ccobo.dev@gmail.com>
2021-09-13 09:06:57 +0100

    fix: added missing views elements and modify js stripe elements file

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JZA4ZBRdfWNdAjQopsn68YAGPIwgNBtswtKE7sFBa2bjDzRaYh4SO6FXbKfhjPZygO0BizNVoKJZEmK9a79NO0600bOs7Q4Vv',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
