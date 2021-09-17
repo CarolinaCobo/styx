40db87deb6fbbbcaff24b1adbeb621a26abef0d5
Carolina Cobo <ccobo.dev@gmail.com>
2021-09-17 19:54:54 +0100

    fix: remove secret key and set up debug

--- styx/settings.py
+++ styx/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cy2n&8c(nryx-&f)y80^e!!!+@=7pas&iu-y$@=^#ayesh855_'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['styx-shoes.herokuapp.com']
 
