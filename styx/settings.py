0111e95e85661d93d65937c495e2c75c1ad69bad
Carolina Cobo <ccobo.dev@gmail.com>
2021-10-12 16:27:10 +0100

    style: fix style on explore h2

--- styx/settings.py
+++ styx/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['styx-shoes.herokuapp.com', '*']
+ALLOWED_HOSTS = ['styx-shoes.herokuapp.com', 'localhost']
 
 
 # Application definition
