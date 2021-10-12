84e04f2d92ab6d67488c7e9864a82f16153564e1
Carolina Cobo <ccobo.dev@gmail.com>
2021-10-12 12:01:31 +0100

    refactor: change name for border radius element for clarity when reading the code

--- profiles/forms.py
+++ profiles/forms.py
@@ -30,5 +30,5 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black border-radius-button  profile-form-input'
+            self.fields[field].widget.attrs['class'] = 'border-black border-radius-default   profile-form-input'
             self.fields[field].label = False
